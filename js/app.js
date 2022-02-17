a8d7f4323754fa7c9bac6936c5d58c93d34231dd
Nazmul Alam <shawonkst15@gmail.com>
2022-02-17 20:18:51 +0600

    exapnse and income budget add

--- js/app.js
+++ js/app.js
@@ -1,54 +1,16 @@
+function myExpanse(){
+     const income = document.getElementById('income-input').value;
+     const salary = parseFloat(income);
+     const foodAmount  = document.getElementById('food-Number').value;
+     const foodName = parseFloat(foodAmount );
+     const RentAmount  = document.getElementById('rent-Number').value;
+     const rentName = parseFloat(RentAmount  );
+     const clothesAmount = document.getElementById('clothes-number').value;
+     const clothes = parseFloat(clothesAmount);
+     const expanseTotal =  document.getElementById('expanse-total').innerHTML=(foodName+rentName+clothes);
+     const salaryRange = document.getElementById('balance-total').innerHTML=(salary- expanseTotal);
+     return salaryRange;
 
-function myfoodfunction(){
-    const foodInput = document.getElementById('food-input');
-    const foodText = foodInput.value;
-    const newFoodAmount = parseFloat(foodText);
-    foodInput.value = '';
-    
-    return newFoodAmount
-    
 }
-// function myRentfunction(){
-//     const rentInput = document.getElementById('rent-input');
-//     const rentText = rentInput.value;
-//     const newRentAmount = parseFloat(rentText);
-//      rentInput.value = '';
-    
-//     return newRentAmount
-    
-// }
 
-
-
-document.getElementById('calculet-btn').addEventListener('click',function(){
-    // get food amount set
-     const myFoodToTal = myfoodfunction();
-     
-    // update  Food total
- const expanseTotal = document.getElementById('expanse-total');
-
- const previousFoodText = expanseTotal.innerText;
- const previousFoodAmount = parseFloat(previousFoodText);
-
- const newFoodTotal = previousFoodAmount + myFoodToTal;
- expanseTotal.innerText = newFoodTotal;
- 
- })
-
-
-
-// document.getElementById('calculet-btn').addEventListener('click',function(){
-//       const myRentTotal = myRentfunction();
-      
-//       const RentTotal = document.getElementById('expanse-total');
-//       const prevRentText = RentTotal.innerText;
-//       const prevRentAmount= parseFloat(prevRentText) ;
-      
-//      const newRent = prevRentAmount + myRentTotal;
-//      RentTotal.innerText=newRent;
-
-//  })
-
- // update rent total
-console.log()
 
--- js/balance.js
+++ js/balance.js
@@ -1,1 +1,59 @@
 
+function myfoodfunction(expanseId){
+    const foodInput = document.getElementById(expanseId);
+    const foodText = foodInput.value;
+    const newFoodAmount = parseFloat(foodText);
+    foodInput.value = '';
+    
+    return newFoodAmount
+    
+}
+
+function myRentfunction(rentId){
+    const rentInput = document.getElementById(rentId);
+    const rentText = rentInput.value;
+    const newRentAmount = parseFloat(rentText);
+     rentInput.value = '';
+    
+    return newRentAmount
+    
+}
+
+
+// update food
+
+document.getElementById('calculet-btn').addEventListener('click',function(){
+    // get food amount set
+     const myFoodToTal = myfoodfunction('food-input');
+    
+     
+    // update  Food total
+ const expanseTotal = document.getElementById('expanse-total');
+
+ const previousFoodText = expanseTotal.innerText;
+ const previousFoodAmount = parseFloat(previousFoodText);
+
+ const newFoodTotal = previousFoodAmount + myFoodToTal;
+ expanseTotal.innerText = newFoodTotal;
+ 
+ })
+
+ //update rent
+//  document.getElementById('calculet-btn').addEventListener('click',function(){
+//      // get 
+//      const myRentTotal = myRentfunction('rent-input');
+
+
+//      //upda rent
+//     const expanseTotal = document.getElementById('expanse-total');
+//     const previousRentText = expanseTotal.innerText;
+//     const previousRentAmount = parseFloat(previousRentText);
+//     const newRentTotal = previousRentAmount + myRentTotal ;
+//     expanseTotal.innerText=newRentTotal;
+ 
+//   })
+
+
+
+
+
